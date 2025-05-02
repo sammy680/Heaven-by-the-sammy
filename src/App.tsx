0b719c442dfdcc5003c916e24858382c7015df2d
sammy <mysam3939@gmail.com>
2025-05-02 19:29:46 +0530

    your commit message

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage"; // 👈 your homepage component
-import ProductPage from "./components/ProductPage"; // 👈 the one with reviews
+import ProductPage from "./pages/ProductPage"; // 👈 the one with reviews
 import TawkToWidget from "./components/TawkToWidget";
 const App: React.FC = () => {
   return (
